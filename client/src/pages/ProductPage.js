4420fa41728ac330e627de7329bdb50f52276ef8
Brent Johnson <brentjohnson3274@gmail.com>
2023-03-09 14:22:34 -0600

    bootstrap button import

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-import {Container, Row, Col} from 'react-bootstrap'
+import {Container, Row, Col, Button} from 'react-bootstrap'
 
 const ProductPage = ({user}) => {
   // Set state for search result and search query
