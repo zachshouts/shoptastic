95bdcf41f9785cb6b931096685465e3ccde122e1
Lon Heligas <lon.heligas@gmail.com>
2023-03-13 11:29:00 -0500

    product data seeds properly

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,17 +1,19 @@
 const connection = require('../config/connection')
 const mongoose = require('mongoose');
 const { Category, Product, User, Order } = require ('../models');
-const { 
-  productData, 
-  userData, 
-  categoryData,
-} = require ('./seeds')
+const productData  = require ('../seeds/productData.json');
+const [ userData ] = require ('../seeds/userData.json');
+const categoryData = require ('../seeds/categoryData.json');
+const { insertMany } = require('../models/User');
 
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
 
   console.log('connected');
+  // console.log([productData]);
+  // console.log([userData]);
+  // console.log([categoryData]);
 
   await Product.deleteMany({});
   await Category.deleteMany({});
@@ -19,14 +21,25 @@ connection.once('open', async () => {
   await Order.deleteMany({});
 
   const products = [];
+  const category = [];
 
-  productData.map((item) => {
+  productData.map( async (item) => {
     const id = new mongoose.Types.ObjectId();
     item = { _id: id, ...item}
-    productIds.push(id);
+    console.log(item)
+    products.push(id);    
+    await Product.collection.insertOne(item)
   })
+  // console.log(products);
+  // console.log([productData])
+
+  // categoryData.map( async () => {
+  //   category = {this.name}
+  //   await Catefory.collection.insertOne()
   
-  
+
+  // })
+
 })
 
 
